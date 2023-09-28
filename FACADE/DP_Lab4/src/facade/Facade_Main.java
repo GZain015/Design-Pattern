1c7823b836bca96a815693f122e95efd78b4c370
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2023-09-28 10:43:09 +0500

    Updated Code

---
+++ FACADE/DP_Lab4/src/facade/Facade_Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+/**
+ *
+ * @author fa20-bse-017
+ */
+import video_conversion.*;
+
+import java.io.File;
+
+public class Facade_Main {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+}
