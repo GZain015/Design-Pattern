69cfc68fd558de05e9e38fd3e4ce6cad7a42ca4c
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2023-10-05 09:16:24 +0500

    Updated Code

---
+++ Design_Patterns/src/GOF/DP_Lab5/Mediator/ChatRoom/MediatorMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_Lab5.Mediator.ChatRoom;
+
+/**
+ *
+ * @author FA20-BSE-017
+ */
+public class MediatorMain {
+    public static void main(String[] args) {
+        User robert = new User("Robert");
+        User john = new User("John");
+
+        robert.sendMessage("Hi! John!");
+        john.sendMessage("Hello! Robert!");
+    }
+}
