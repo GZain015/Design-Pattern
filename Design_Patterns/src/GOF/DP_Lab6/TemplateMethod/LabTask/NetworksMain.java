89cf7c483ca78257ecd322e7c746707b7e43b751
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2023-10-15 12:55:41 +0500

    Update Code

--- Design_Patterns/src/GOF/DP_Lab6/TemplateMethod/LabTask/NetworksMain.java
+++ Design_Patterns/src/GOF/DP_Lab6/TemplateMethod/LabTask/NetworksMain.java
@@ -9,7 +9,7 @@ package GOF.DP_Lab6.TemplateMethod.LabTask;
  * @author Zain
  */
 
-//import GOF.DP_Lab6.TemplateMethod.LabTask.*;
+import GOF.DP_Lab6.TemplateMethod.LabTask.*;
 //import GOF.DP_Lab6.TemplateMethod.LabTask.Facebook;
 //import GOF.DP_Lab6.TemplateMethod.LabTask.Network;
 //import GOF.DP_Lab6.TemplateMethod.LabTask.Twitter;
@@ -24,34 +24,93 @@ import java.io.InputStreamReader;
 public class NetworksMain {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        GOF.DP_Lab6.TemplateMethod.Networks.Network network = null;
+//        Network network = null;
         System.out.print("Input user name: ");
         String userName = reader.readLine();
         System.out.print("Input password: ");
         String password = reader.readLine();
+        
+        Boolean check = true;
 
-        // Enter the message.
-        System.out.print("Input message: ");
-        String message = reader.readLine();
+        do{
+            // Enter the message.
+            System.out.print("Enter message: ");
+            String messageDetails = reader.readLine();
+            
+            System.out.println("Choose message type:\n"+ 
+                    "1 - ERROR\n" +
+                    "2 - WARNING\n" +
+                    "3 - INFO"
+            );
+            int messageTypeChoice = Integer.parseInt(reader.readLine());
+          
+            MessageType messageType = MessageType.ERROR;
+            
+            if(messageTypeChoice == 1){
+                messageType = MessageType.ERROR;
+            }else if(messageTypeChoice == 2){
+                messageType = MessageType.WARNING;
+            }else if(messageTypeChoice == 3){
+                messageType = MessageType.INFO;
+            }else{
+                messageType = MessageType.ERROR;
+            }
 
-        System.out.println("\nChoose social network for posting message.\n" +
-                "1 - Facebook\n" +
-                "2 - Twitter\n" +
-                "3 - LinkedIn" 
+            System.out.println("\nChoose social network for posting message.\n" +
+                    "1 - Facebook\n" +
+                    "2 - Twitter\n" +
+                    "3 - LinkedIn\n" +
+                    "4 - Leave empty for all...." 
+            );
+            int Networkchoice = Integer.parseInt(reader.readLine());
+            String networkTypeInput;
+            // Create proper network object and send the message.
+            if (Networkchoice == 1) {
+                networkTypeInput = "Facebook";
+            } else if (Networkchoice == 2) {
+                networkTypeInput = "Twitter";
+            } else if (Networkchoice == 3) {
+                networkTypeInput = "LinkedIn";
+            } else {
+                networkTypeInput = "";
+            }
+            
+            NetworkType networkType = (networkTypeInput.isEmpty()) ? null : NetworkType.valueOf(networkTypeInput);
+             
+            Message message = new Message(messageDetails, messageType, networkType);
+            Network network = null;
+            
+            
+            if (message.getNetworkType() == null) {
+                network = new Facebook(userName, password);
+                //network.post(message.getDescription());
+                network.post(message);
+                network = new Twitter(userName, password);
+                network.post(message);
+                network = new LinkedIn(userName, password);
+                network.post(message);
+            } else {
+                if (networkTypeInput == "Facebook") {
+                    network = new Facebook(userName, password);
+                } else if (networkTypeInput == "Twitter") {
+                    network = new Twitter(userName, password);
+                } else if (networkTypeInput == "LinkedIn") {
+                    network = new LinkedIn(userName, password);
+                }
+            }
+            
+            
+            
+//            network.post(message);
+            
+            System.out.print("Do you want to Continue (Y/N)? ");
+            String continueInput = reader.readLine();
+            if (!continueInput.equalsIgnoreCase("Y")) {
+                check = false;
+                break;
                 
-        );
-        int choice = Integer.parseInt(reader.readLine());
-
-        // Create proper network object and send the message.
-        if (choice == 1) {
-            network = new GOF.DP_Lab6.TemplateMethod.Networks.Facebook(userName, password);
-        } else if (choice == 2) {
-            network = new GOF.DP_Lab6.TemplateMethod.Networks.Twitter(userName, password);
-        } else if (choice == 3) {
-            network = new GOF.DP_Lab6.TemplateMethod.Networks.LinkedIn(userName, password);
-        } else {
-            System.out.println("\nInvalid Option...");
-        }
-        network.post(message);
+            }
+            
+        }while(check);
     }
 }
