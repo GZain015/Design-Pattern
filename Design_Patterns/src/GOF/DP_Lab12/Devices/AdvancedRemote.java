2357998c46e6273cd3885283076d249611ee47ba
Ghulam Zain <85392231+GZain015@users.noreply.github.com>
2023-12-21 04:40:03 +0500

    Lab11 Done

---
+++ Design_Patterns/src/GOF/DP_Lab12/Devices/AdvancedRemote.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_Lab12.Devices;
+
+/**
+ *
+ * @author fa20-bse-017
+ */
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ Design_Patterns/src/GOF/DP_Lab12/DrawAPI/BridgeMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.DP_Lab12.DrawAPI;
+
+/**
+ *
+ * @author fa20-bse-017
+ */
+public class BridgeMain {
+    public static void main(String[] args) {
+      Shape redCircle = new Circle(100,100, 10, new RedCircle());
+      Shape greenCircle = new Circle(100,100, 10, new GreenCircle());
+
+      redCircle.draw();
+      greenCircle.draw();
+   }
+}
